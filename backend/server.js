99db73255f03e82adf0dfee86e47026b3e3fde04
ramirolepori <leporiramiro@gmail.com>
2023-06-07 15:25:23 -0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./src/lib/DBprov.js');
 
 // Rutas
 app.get('/', (req, res) => {
   res.send('¡Hola desde el backend!');
+});
+
+// Ruta para obtener los proyectos desde la base de datos
+app.get('/projects', async (req, res) => {
+  try {
+    const projects = await db.getProjects();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los proyectos' });
+  }
 });
 
 // Iniciar el servidor
---
+++ backend/src/lib/DBprov.js
@@ -0,0 +1,44 @@
+// DBprov.js
+
+const mongoose = require('mongoose');
+
+// Configuración de la conexión a la base de datos
+const dbUrl = 'mongodb+srv://ramirolepori:FLqSiz7UVwIeX2NP@myweb.gzp1hef.mongodb.net/myWeb?retryWrites=true&w=majority'; // URL de conexión de MongoDB Atlas
+
+// Conexión a la base de datos
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
+
+// Definir el esquema de tu colección
+const projectSchema = new mongoose.Schema({
+  name: String,
+  picture: String,
+  description: String,
+  url: String,
+  fechaCreacion: { type: Date, default: Date.now }
+});
+
+// Definir el modelo basado en el esquema
+const Project = mongoose.model('Project', projectSchema);
+
+// Función para obtener todos los proyectos
+async function getProjects() {
+  try {
+    const projects = await Project.find();
+    return projects;
+  } catch (error) {
+    console.error('Error al obtener los proyectos:', error);
+    throw error;
+  }
+}
+
+// Exportar la instancia de conexión y la función getProjects para poder utilizarlas en otros archivos
+module.exports = {
+  connection: mongoose.connection,
+  getProjects
+};
