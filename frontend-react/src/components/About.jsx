aef4f2617f4d8fc8e3c4dcb316a1f62298d4112f
ramirolepori <leporiramiro@gmail.com>
2023-09-20 15:18:44 -0300

    .

--- frontend-react/src/components/About.jsx
+++ frontend-react/src/components/About.jsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import '../assets/css/about.css'; // Ajusta la ruta del archivo CSS según tu estructura de archivos
 import { useTranslation } from "react-i18next";
 import './i18n'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 
 const About = () => {
   const { t } = useTranslation();
@@ -12,7 +19,7 @@ const About = () => {
   useEffect(() => {
     // Reemplaza la URL con la ruta de tu servidor Node.js que devuelve los datos JSON
     const apiUrl = "http://localhost:3000/certificates";
-    
+
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => setCertificates(data))
@@ -33,18 +40,35 @@ const About = () => {
           <p>{t("pAbout7")}</p>
         </div>
         <div className="botton-container">
-
         </div>
       </div>
+
       <h2>{t("h2Certifications1")}</h2>
-      <div className="certifications-container">
-        {certificates.map(certificate => (
-          <div key={certificate._id} className="certification-item">
-            <img src={`http://localhost:3000/images/${certificate.image}`} alt={certificate.name} />
-            <p className="project-title">{certificate.description}</p>
-          </div>
-        ))}
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          spaceBetween={10}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+        >
+          {certificates.map(certificate => (
+            <SwiperSlide className='slide' key={certificate._id}>
+                <img src={`http://localhost:3000/images/${certificate.image}`} alt={certificate.name} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
+
     </section>
   );
 };
--- frontend-react/src/components/Certifications.jsx
+++ frontend-react/src/components/Certifications.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import '../assets/css/certifications.css'
+import SwiperCore, { EffectCoverflow, Navigation } from 'swiper/core';
+import 'swiper/swiper-bundle.min.css'; // Importa los estilos de Swiper
+import '../assets/css/certifications.css';
+
+// Instala los módulos necesarios de Swiper
+SwiperCore.use([EffectCoverflow, Navigation]);
 
 const Certifications = () => {
   const [certificates, setCertificates] = useState([]);
 
   useEffect(() => {
-    // Reemplaza la URL con la ruta de tu servidor Node.js que devuelve los datos JSON
     const apiUrl = "http://localhost:3000/certificates";
 
     fetch(apiUrl)
@@ -14,16 +18,41 @@ const Certifications = () => {
       .catch(error => console.error("Error al obtener los datos:", error));
   }, []);
 
+    useEffect(() => {
+    // Después de cargar los certificados, inicializa Swiper
+    const swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }, []);
+
   return (
     <section id="certifications" className="section">
       <h2>My Certifications</h2>
-      <div className="certifications-container">
-        {certificates.map(certificate => (
-          <div key={certificate._id} className="certification-item">
-            <img src={`http://localhost:3000/images/${certificate.image}`} alt={certificate.name} />
-            <p className="project-title">{certificate.description}</p>
-          </div>
-        ))}
+      <div className="swiper-container">
+        <div className="swiper-wrapper">
+          {certificates.map(certificate => (
+            <div key={certificate._id} className="swiper-slide">
+              <img src={`http://localhost:3000/images/${certificate.image}`} alt={certificate.name} />
+              <p className="project-title">{certificate.description}</p>
+            </div>
+          ))}
+        </div>
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
       </div>
     </section>
   );
